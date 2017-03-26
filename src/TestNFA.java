53145e6de0c3658184955e9dca0389042a872f86
Rahul Thapar <rahthap@gmail.com>
2017-03-26 11:24:56 +0530

    deleted layout.java

--- src/TestNFA.java
+++ src/TestNFA.java
@@ -7,7 +7,7 @@ public class TestNFA {
     static char[] array = new char[50];
     static int str_length;
     static char temp;
-    static String string;
+    static String string, new_string;
     static char[] result_con = new char[str_length];
 
     public static void main(String[] args) {
@@ -20,64 +20,55 @@ public class TestNFA {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter String : ");
         string = sc.nextLine();
-        str_length = string.length();
+        new_string = string.replaceAll("\\w{2}", "$0.");
+       System.out.println(string.replaceAll("\\w{2}", "$0."));
+        str_length = new_string.length();
 
-        System.out.println("Entered String : " + string + " of length :" + str_length);
+
+        System.out.println("Entered String : " + new_string + " of length :" + str_length);
 
         for (int i = 0; i < str_length; i++) {
-            array[i] = string.charAt(i);
+            array[i] = new_string.charAt(i);
         }
 
         //Array contents : Check the alphabets in the string
         for (int i = 0; i < str_length; i++) {
-            /*char charAt2 = string.charAt(i);
-            if (Character.isLetter(charAt2)) {
-                count++;
-                charArray[k]=array[i];
-                k++;*/
                 System.out.println("Index :" + i + " = " + array[i]);
-                //System.out.println(string.replaceAll("(\\d)(\\d)", "$1.$2"));
-            //}
         }
 
+        //---------------------------------------------------------
         //CharArray containing all the alphabets in the expression
-       /*for (int i=0;i<count;i++){
+
+         /*for (int i=0;i<count;i++){
            System.out.println(" "+charArray[i]);
        }*/
 
-        /*
-        * Put concatenation operator :
-        * ab
-        * a(
-         * )a
-         * *a
-         * *(
-         * )(.
-         *
-        * */
-        //System.out.println(string.replaceAll("ab","a.b"));
+        //---------------------------------------------------------
+
+
+
+        //------------------------------------------------------
+        //        STEP 1 : Put concatenation operator(. Dot)
+        //                 wherever concatenation is required.
+        //-------------------------------------------------------
+
+
         for (int i=0;i<str_length;i++){
-            if (Character.isLetter(array[i])&&Character.isLetter(array[i+1])) {
-                result_con=Concatenate_ab(array,i,'.');
+            if (Character.isLetter(array[i])&&Character.isLetter(array[i+1])) //Check if there are 2 consecutive alphabets
+            {
+               // System.out.println("2 consecutive alphabets : "+array[i]+" and "+ array[i+1]);
+                //Concatenate(array, i);
             }
         }
         for (int i=0;i<result_con.length;i++){
             System.out.print(result_con[i]);
         }
+    }
+
+    private static void Concatenate(char[] array, int pos) {
+
+        /* ab : a - pos and b - pos+1 */
 
 
     }
-
-    private static char[] Concatenate_ab(char[] array, int pos, char charc) {
-        char[] result = new char[string.length()];
-        for(int i = 0; i < pos; i++)
-            result[i] = array[i];
-        result[pos] = charc;
-        for(int i = pos + 1; i < array.length; i++)
-            result[i] = array[i - 1];
-        return result;
-
-    }
-
-
 }
